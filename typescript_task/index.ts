3caacaf2b61698806ccc80b6f550a66a15f1fc69
ajay-r-16 <77270202+ajay-r-16@users.noreply.github.com>
2021-03-07 09:08:56 +0530

    Add files via upload

---
+++ typescript_task/index.ts
@@ -0,0 +1,32 @@
+function leapYear(year:number):boolean{
+    if(year % 4 ===0){
+        if(year % 100 ===0){
+            if(year % 400 ===0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return true;
+        }
+    }
+    else{
+        return false;
+    }
+}
+
+
+function revString(str:string):string{
+    let revstr:string="";
+    let n = str.length - 1;
+    for(let i=n ; i>=0 ; i--){
+        revstr = revstr + str[i];
+    }
+    return revstr;
+}
+
+
+console.log( leapYear(2020) );
+console.log( revString("Hello") );
